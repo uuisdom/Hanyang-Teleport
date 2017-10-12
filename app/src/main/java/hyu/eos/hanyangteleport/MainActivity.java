764698fe03a0765c259121bd3873d58fdee142ec
HamJiSeong <hgs3896@naver.com>
2017-10-12 19:53:28 +0900

    Add Main Activity java file

---
+++ app/src/main/java/hyu/eos/hanyangteleport/MainActivity.java
@@ -0,0 +1,16 @@
+package hyu.eos.hanyangteleport;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by hgs on 2017-10-12.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
